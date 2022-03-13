2839fab14573f0a7689c87817b8493a2bfecc9a4
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-03-13 16:55:59 +0100

    Update dom_basic.js

--- dom_basic.js
+++ dom_basic.js
@@ -70,4 +70,41 @@ const dollar = document.querySelector('body input.dollar');
         dollar.value = '';
         mad.value = '';
     });
-/****************************************************/
+/****************************************************/  //Exercice__3 addEventListner('eventName',functionName||functionAnonimous) & removeEventListner('eventName remove',functionName)
+
+<div class="myDiv" style="background-color: coral;color: #000;padding: 40px;">
+  <h3 class="title">JavaScript removeEventListener</h3>
+  <p>This div element has an onmousemove event handler that displays a random number every time you move your mouse inside this orange field.</p>
+  <p>Click the button to remove the div's event handler.</p>
+  <button class="btn_Star">Start function</button>
+  <button class="btn_Pause">Pause function</button>
+</div>
+<p class="demo" style="background: yellow;font-size: 18px;width: 21px;height:21px;"></p>
+ 
+const myDiv = document.querySelector('body div.myDiv');
+const btns = document.querySelectorAll('body div.myDiv button');   
+const demo = document.querySelector('body div.myDiv + p.demo');   
+const btnStar = btns[0];
+const btnPause = btns[1];
+for(let i = 0;i<btns.length;i++){
+    btns[i].style.setProperty(`cursor`,`pointer`);  //style for buttons
+};
+function mouseMove(){
+    demo.textContent = Math.round(Math.random() * 100);
+};
+window.addEventListener('load',function(){
+    myDiv.removeEventListener('mousemove',mouseMove);
+});
+myDiv.addEventListener('mousemove',mouseMove);
+btnPause.addEventListener('click',function(){
+    myDiv.removeEventListener('mousemove',mouseMove);
+    myDiv.onmouseout = demo.textContent = '';
+});
+btnStar.addEventListener('click',function(){
+    myDiv.addEventListener('mousemove',mouseMove);
+});
+myDiv.addEventListener('mouseout',function(){        //addEventListener() ila brina ga3 les methods tkhdmo en meme temps maxi bha onclick(),... 
+    demo.textContent = '';
+});
+
+/*******************/   
